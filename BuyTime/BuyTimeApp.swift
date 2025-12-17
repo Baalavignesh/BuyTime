3b48224292de929dc6a5638600677b26cb34c19e
Baalavignesh <baalavignesh21@gmail.com>
2025-12-17 13:32:20 -0500

    Initial Commit

---
+++ BuyTime/BuyTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyTimeApp.swift
+//  BuyTime
+//
+//  Created by Baalavignesh Arunachalam on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct BuyTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuyTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BuyTime
+//
+//  Created by Baalavignesh Arunachalam on 12/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
